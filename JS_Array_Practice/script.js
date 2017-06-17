3a2a2217e8ad426e77dd0904c80907a84857400e
Annie Wong <annie.hs.wong@gmail.com>
2017-06-17 11:27:31 -0700

    added array.html

---
+++ JS_Array_Practice/script.js
@@ -0,0 +1,51 @@
+var myArrayA = ["one", "two", "three", "four", 6, 7, 8, 9, 10, false, true]; //implicit literal array
+var myArrayD = ["Kim", "Honda", "jones", "Coffee", false, true]; //implicit literal array
+// console.log(myArrayA);
+//
+// var myArrayB = new Array("one", "two", "three", "four", 6, 7, 8, 9, 10, false, true);
+// console.log(myArrayB);
+//
+// var myArrayC = new Array(5); // run time array
+// myArrayC.push("FIVE");
+// console.log(myArrayC);
+//
+// console.log(myArrayA, myArrayA.length);
+// console.log(myArrayB, myArrayB.length);
+// console.log(myArrayC, myArrayC.length);
+//
+// console.log(typeof(myArrayA) == typeof(myArrayC)); //evaluates true
+//
+// myArrayA.pop(); //remove the last item from the array
+// console.log(myArrayA);
+//
+// myArrayA.shift(); //remove the first item from the array
+// console.log(myArrayA);
+//
+// myArrayA[3]="FOUR";
+// console.log(myArrayA);
+//
+
+// console.log(myArrayA.indexOf("FOUR"));
+
+// myArrayA.splice(2,2); //remove the element w/ index of 2 and remove 2 elements (2,2)
+// console.log(myArrayA);
+// myArrayA.unshift("THREE"); //adding a new element to the front of the array
+// console.log(myArrayA);
+
+// myArrayA.sort();
+// console.log(myArrayA.sort());
+
+// myArrayA.reverse();
+// console.log(myArrayA.reverse());
+
+// var myArrayE = myArrayA.concat(myArrayD);
+// console.log(myArrayE, myArrayE.length);
+//
+// console.log(myArrayA.toString()); //converting to one big string
+// console.log(myArrayD.toString());
+// console.log(myArrayE.toString());
+
+// console.log(myArrayA.join(" + ")); //replaces the comma with +
+
+console.log(myArrayD);
+console.log(myArrayD.slice(2,4)); //starting at the index of 2 and stopping at index 4
